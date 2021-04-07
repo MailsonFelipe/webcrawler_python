539da711795998916943fc51ab1e94743553d53f
MailsonFelipe <mfsm@icomp.ufam.edu.br>
2021-04-07 06:13:59 +0000

    tentando pegar o telefone

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # webcrawler_python
+
+Foi utilizado no projeto a biblioteca "requests".
+
+Observando que daria para ter mais de uma logo, decidi fazer uma lista de URLs para este projeto.
--- crawler.py
+++ crawler.py
@@ -1,5 +1,7 @@
 import requests
-
+import re
+#https://www.illion.com.au/
+#https://www.phosagro.com/contacts/
 site = 'https://www.phosagro.com/contacts/'
 
 resrequisicao = requests.get(site)
@@ -7,24 +9,40 @@ a = resrequisicao.text
 
 l = []
 l = (a.split('\n'))
-link = ''
+linkExtraido = ''
+listaDeLogos = []
 
 for i in range(len(l)):
     if('logo' in l[i] and 'src="' in l[i]):
-        link += l[i]
+        linkExtraido += l[i]
+        linkExtraido = linkExtraido.split('src="')[1].split('"')[0]
+
+        if (not linkExtraido.startswith('http')):
+            linkExtraido = linkExtraido[1:]
+            cont = 0
+            aux = ""
+            for i in range (len(site)):
+                if (cont == 3):
+                    break
+                else:
+                    aux  +=  site[i]
+                    if(site[i] == '/'):
+                        cont += 1
+            linkExtraido = aux + linkExtraido
+        listaDeLogos.append(linkExtraido)
+print(listaDeLogos)
+
+
+for i in range(len(l)):
+    if(l[i].lower().startswith('<') and 'href="' in l[i].lower() and 'contact' in l[i].lower() or 'phone' in l[i].lower()) and re.search('\d', l[i]):
+        numero = l[i].split('<')
+
+        print(numero)
+        i = 0
+        for i in range(len(numero)):
+            if('phone' in numero[i].lower() or 'conctact' in numero[i].lower()):
+                numero = numero[i+1].split('>')[1]
+                break
+                
+        print(numero)
         break
-
-link = link.split('src="')[1].split('"')[0][1:]
-
-if (not link.startswith('http')):
-    cont = 0
-    completeLink = ""
-    for i in range (len(site)):
-        if (cont == 3):
-            break
-        else:
-            completeLink  +=  site[i]
-            if(site[i] == '/'):
-                cont += 1
-completeLink += link
-print(completeLink)
